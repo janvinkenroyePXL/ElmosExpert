52b7356e5edbf1e6387b276fb0c90aa82ea337ff
Jan Vinkenroye <11903691@student.pxl.be>
2023-01-17 22:11:10 +0100

    feat: cell assignment takes into account prison capacity

--- OutOfTheBox.Logic/Services/CellAssignmentService.cs
+++ OutOfTheBox.Logic/Services/CellAssignmentService.cs
@@ -6,24 +6,32 @@ namespace OutOfTheBox.Logic.Services
 {
     public class CellAssignmentService : ICellAssignmentService
     {
-        private readonly ICellRepository _repository;
+        private readonly ICellRepository _cellRepository;
+        private readonly IPrisonRepository _prisonRepository;
 
-        public CellAssignmentService(ICellRepository repository)
+        public CellAssignmentService(ICellRepository cellRepository, IPrisonRepository prisonRepository)
         {
-            _repository = repository;
+            _cellRepository = cellRepository;
+            _prisonRepository = prisonRepository;
         }
 
         public async Task<Cell?> AssignCellToPrisoner(Prisoner prisoner)
         {
-            var freeNonIsolationCells = await _repository.GetFreeNonIsolationCellsAsync();
+            var freeNonIsolationCells = await _cellRepository.GetFreeNonIsolationCellsAsync();
             if(freeNonIsolationCells == null)
             {
                 return null;
             }
-            else
+            else 
             {
+                var freeCellsInFreePrisons = freeNonIsolationCells
+                    .Where(c => _prisonRepository.GetNumberOfPrisonersAsync(c.PrisonId) < c.Prison!.Capacity);
                 var r = new Random();
-                return freeNonIsolationCells.ElementAt(r.Next(0, freeNonIsolationCells.Count()));
+                if (!freeCellsInFreePrisons.Any())
+                {
+                    return freeNonIsolationCells.ElementAt(r.Next(0, freeNonIsolationCells.Count()));
+                }
+                return freeCellsInFreePrisons.ElementAt(r.Next(0, freeCellsInFreePrisons.Count()));
             }
         }
     }
